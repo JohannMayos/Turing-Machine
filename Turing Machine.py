afa439c374f6f81b0a4be53f8dc36b3f346ff396
Johann Mayos Lopes da Silva <42842871+JohannMayos@users.noreply.github.com>
2022-10-12 21:15:17 -0400

    inputs added

--- Turing Machine.py
+++ Turing Machine.py
@@ -1,17 +1,7 @@
-def state_def(name, index):
-    tp = (name, index)
-    states.append(tp)
-    return states
+import ast
 
+machine = input()
 
-machine = {}
-states = []
+inputs = ast.literal_eval(machine)
 
-for i in range(3):
-    name = input()
-    index = int(input())
-    state_def(name, index)
-
-
-machine = dict(states)
-print(machine)
+print(inputs)
